291a89ed2b44a8a51bfe3dfeb1378cb7c8b7f20b
Mehak <chandnamehak1102@example.com>
2026-04-22 14:50:24 +0530

    initial

--- client/src/lib/api.js
+++ client/src/lib/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import toast from "react-hot-toast";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  // baseURL: "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL, 
 });
 
 // attach token
